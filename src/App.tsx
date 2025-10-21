e2461b49afbca09ef4ac398ea0df820942c4bd1c
XRlayth <sh4d0r@interia.pl>
2025-10-21 05:01:01 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ export default function App() {
         <ContactForm />
         <ReviewsCarousel />
         <WhyUs />
-        <ProcessTimeline />
         <Portfolio />
         <FAQ />
         <CTASection />
